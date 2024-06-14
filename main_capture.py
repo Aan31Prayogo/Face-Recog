bbe87be59a09a10235a4c0501de462082b13f1cc
Yulian Surya Prayogo <yulian@cognizest.com>
2024-06-14 14:47:25 +0700

    update: add display text on webcam

--- main_capture.py
+++ main_capture.py
@@ -7,15 +7,24 @@ import time
 from dotenv import load_dotenv
 
 load_dotenv()
+
 # Path to the known image
 IMAGE_NAME = os.getenv('IMAGE_NAME')
-PATH_IMAGE = os.path.join(sys.path[0] + IMAGE_NAME)
+PATH_IMAGE = os.path.join(sys.path[0] + "/" +IMAGE_NAME)
 TIMER = 5
+
+#load model
 known_image = face_recognition.load_image_file(PATH_IMAGE)
 known_encoding = face_recognition.face_encodings(known_image)[0]
 
+#flaging
 flag_process_frame = True
 flag_stop = False
+
+#color
+GREEN = (0,255,0)
+RED = (0,0,255)
+FONT = cv2.FONT_HERSHEY_SIMPLEX
 
 def start_timer_flag():
     threading.Thread(target=timer_flag).start()
@@ -42,6 +51,7 @@ def timer_flag():
 def main():
     global flag_process_frame
     global flag_stop
+    status = ""
     
     cap = cv2.VideoCapture(0)
     while True:
@@ -53,18 +63,21 @@ def main():
         if flag_process_frame:
             print("[INFO] start process image")
             face_locations = face_recognition.face_locations(rgb_small_frame)
-            print(face_locations, type(face_locations))
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
+            
             for face_encoding in face_encodings:
-                # Compare the face with the known face
                 matches = face_recognition.compare_faces([known_encoding], face_encoding)
                 if True in matches:
                     print("[DEBUG] Valid person")
+                    status = "VALID PERSON"
+                    color = GREEN
                 else:
                     print("[DEBUG] Invalid person")
+                    status = "INVALID PERSON"
+                    color = RED
             flag_process_frame = False
 
+        cv2.putText(frame, status, (10,40), FONT, 1, color, 2)
         cv2.imshow('Video', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
